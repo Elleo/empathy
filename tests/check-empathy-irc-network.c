3e71691602735f9210eb8e6389fc8bb97b479e8f
Guillaume Desmottes <gdesmott@gnome.org>
2008-01-06 20:17:00 +0100

    check-empathy-irc-network.c: test empathy_irc_network_remove_server

--- tests/check-empathy-irc-network.c
+++ tests/check-empathy-irc-network.c
@@ -147,6 +147,48 @@ START_TEST (test_add_server)
       g_free (address);
     }
 
+  /* Now let's remove the 3rd server */
+  l = g_slist_nth (servers, 2);
+  fail_if (l == NULL);
+  server = l->data;
+  empathy_irc_network_remove_server (network, server);
+
+  /* free the list */
+  g_slist_foreach (servers, (GFunc) g_object_unref, NULL);
+  g_slist_free (servers);
+
+  /* Request the new servers list */
+  servers = empathy_irc_network_get_servers (network);
+  fail_if (g_slist_length (servers) != 3);
+
+  /* The 3rd server should have disappear */
+  for (l = servers, i = 0; l != NULL; l = g_slist_next (l), i++)
+    {
+      gchar *address;
+      guint port;
+      gboolean ssl;
+
+      server = l->data;
+
+      g_object_get (server,
+          "address", &address,
+          "port", &port,
+          "ssl", &ssl,
+          NULL);
+
+      /* 3rd server was removed */
+      if (i == 2)
+        i++;
+
+      fail_if (address == NULL || strcmp (address, test_servers[i].address)
+          != 0);
+      fail_if (port != test_servers[i].port);
+      fail_if (ssl != test_servers[i].ssl);
+
+      g_free (address);
+    }
+
+  /* free the list */
   g_slist_foreach (servers, (GFunc) g_object_unref, NULL);
   g_slist_free (servers);
 
