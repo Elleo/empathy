a95a12779fc5c937f25a6a0c4a768e17776d6076
Guillaume Desmottes <gdesmott@gnome.org>
2008-01-06 15:26:20 +0100

    test empathy_irc_network_new

--- tests/check-empathy-irc-network.c
+++ tests/check-empathy-irc-network.c
@@ -8,9 +8,31 @@
 
 #include <libempathy/empathy-irc-network.h>
 
+START_TEST (test_empathy_irc_network_new)
+{
+  EmpathyIrcNetwork *network;
+  gchar *id = NULL, *name = NULL;
+
+  network = empathy_irc_network_new ("id1", "Network1");
+
+  g_object_get (network,
+      "id", &id,
+      "name", &name,
+      NULL);
+
+  fail_if (id == NULL || strcmp (id, "id1") != 0);
+  fail_if (name == NULL || strcmp (name, "Network1") != 0);
+
+  g_free (id);
+  g_free (name);
+  g_object_unref (network);
+}
+END_TEST
+
 TCase *
 make_empathy_irc_network_tcase (void)
 {
     TCase *tc = tcase_create ("empathy-irc-network");
+    tcase_add_test (tc, test_empathy_irc_network_new);
     return tc;
 }
